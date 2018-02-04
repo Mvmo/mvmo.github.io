987522a51477ff45982e68c43cf47f3c00e74233
Mvmo <entchenmomoyt@gmail.com>
2018-02-04 16:36:30 +0100

    first version of the page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(window).on("load", function () {
+    swal({
+        icon: "info",
+        title: "Entwicklung",
+        text: "Diese Website befindet sich noch in der Entwicklung",
+        buttons: {
+            okay: "Okay"
+        }
+    });
+});
